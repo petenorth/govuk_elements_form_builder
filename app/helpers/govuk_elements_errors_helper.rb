cd9c9f99a1ce80e60dd591be60dafb50d5ce507b
Stephen Richards <stephen.richards@digital.justice.gov.uk>
2017-03-08 17:15:54 +0000

    Trying to get this bloody thing to work

--- app/helpers/govuk_elements_errors_helper.rb
+++ app/helpers/govuk_elements_errors_helper.rb
@@ -6,8 +6,13 @@ module GovukElementsErrorsHelper
   end
 
   def self.error_summary object, heading, description
+    puts ">>>>>>>>>>>>>>  #{__FILE__}:#{__LINE__} <<<<<<<<<<<<<<<<<\n"
+    # @object_ids = Set.new
+    puts ">>>>>>>>>>>>>>  #{__FILE__}:#{__LINE__} <<<<<<<<<<<<<<<<<\n"
     return unless errors_exist? object
+    puts ">>>>>>>>>>>>>>  #{__FILE__}:#{__LINE__} <<<<<<<<<<<<<<<<<\n"
     error_summary_div do
+      puts ">>>>>>>>>>>>>>  #{__FILE__}:#{__LINE__} <<<<<<<<<<<<<<<<<\n"
       error_summary_heading(heading) +
       error_summary_description(description) +
       error_summary_list(object)
@@ -19,24 +24,35 @@ module GovukElementsErrorsHelper
   end
 
   def self.child_errors_present? object
+    @object_ids = Set.new
     attributes(object).any? { |child| errors_exist?(child) }
   end
 
   def self.attributes object
     child_objects = attribute_objects object
-    nested_child_objects = child_objects.map { |o| attributes(o) }
+    nested_child_objects = child_objects.reject { |o| @object_ids.include?(o) }.map { |o| @object_ids << o; attributes(o) }
     (child_objects + nested_child_objects).flatten
   end
 
   def self.attribute_objects object
-    object.
+    @object_ids = Set.new
+
+    attr_objects = object.
       instance_variables.
       map { |var| instance_variable(object, var) }.
       compact
+    # don't return any objects that we've seen before
+    # attr_objects.delete_if { |ao| @object_ids.include?(ao.object_id) }
+    # @object_ids.merge attr_objects.map(&:object_id)
+    attr_objects
   end
 
   def self.child_to_parent object, parents={}
-    attribute_objects(object).each do |child|
+
+    aos = attribute_objects(object)
+    aos.reject { |o| @objects.include?(o) }.each do |child|
+    # attribute_objects(object).each do |child|
+      @objects << child
       parents[child] = object
       child_to_parent child, parents
     end
@@ -53,6 +69,7 @@ module GovukElementsErrorsHelper
   end
 
   def self.children_with_errors object
+    @object_ids = Set.new
     attributes(object).select { |child| errors_present?(child) }
   end
 
@@ -80,11 +97,20 @@ module GovukElementsErrorsHelper
   end
 
   def self.error_summary_list object
+    puts ">>>>>>>>>>>>>>  #{__FILE__}:#{__LINE__} <<<<<<<<<<<<<<<<<\n"
     content_tag(:ul, class: 'error-summary-list') do
+      puts ">>>>>>>>>>>>>>  #{__FILE__}:#{__LINE__} <<<<<<<<<<<<<<<<<\n"
+      # @object_ids = Set.new
+      puts ">>>>>>>>>>>>>>  #{__FILE__}:#{__LINE__} <<<<<<<<<<<<<<<<<\n"
+      @objects = Set.new
       child_to_parents = child_to_parent(object)
+      puts ">>>>>>>>>>>>>>  #{__FILE__}:#{__LINE__} <<<<<<<<<<<<<<<<<\n"
       messages = error_summary_messages(object, child_to_parents)
-
+      puts ">>>>>>>>>>>>>>  #{__FILE__}:#{__LINE__} <<<<<<<<<<<<<<<<<\n"
+      # @object_ids = Set.new
+      puts ">>>>>>>>>>>>>>  #{__FILE__}:#{__LINE__} <<<<<<<<<<<<<<<<<\n"
       messages << children_with_errors(object).map do |child|
+        puts ">>>>>>>>>>>>>>  #{__FILE__}:#{__LINE__} <<<<<<<<<<<<<<<<<\n"
         error_summary_messages(child, child_to_parents)
       end
 
@@ -93,19 +119,27 @@ module GovukElementsErrorsHelper
   end
 
   def self.error_summary_messages object, child_to_parents
+    puts ">>>>>>>>>>>>>>  #{__FILE__}:#{__LINE__} <<<<<<<<<<<<<<<<<\n"
     object.errors.keys.map do |attribute|
+      puts ">>>>>>>>>>>>>>  #{__FILE__}:#{__LINE__} <<<<<<<<<<<<<<<<<\n"
       error_summary_message object, attribute, child_to_parents
     end
   end
 
   def self.error_summary_message object, attribute, child_to_parents
+    puts ">>>>>>>>>>>>>>  #{__FILE__}:#{__LINE__} <<<<<<<<<<<<<<<<<\n"
     messages = object.errors.full_messages_for attribute
+    puts ">>>>>>>>>>>>>>  #{__FILE__}:#{__LINE__} <<<<<<<<<<<<<<<<<\n"
     messages.map do |message|
+      puts ">>>>>>>>>>>>>>  #{__FILE__}:#{__LINE__} <<<<<<<<<<<<<<<<<\n"
       object_prefixes = object_prefixes object, child_to_parents
+      puts ">>>>>>>>>>>>>>  #{__FILE__}:#{__LINE__} <<<<<<<<<<<<<<<<<\n"
       link = link_to_error(object_prefixes, attribute)
+      puts ">>>>>>>>>>>>>>  #{__FILE__}:#{__LINE__} <<<<<<<<<<<<<<<<<\n"
       message.sub! default_label(attribute), localized_label(object_prefixes, attribute)
       content_tag(:li, content_tag(:a, message, href: link))
     end
+    puts ">>>>>>>>>>>>>>  #{__FILE__}:#{__LINE__} <<<<<<<<<<<<<<<<<\n"
   end
 
   def self.link_to_error object_prefixes, attribute
@@ -126,19 +160,27 @@ module GovukElementsErrorsHelper
   end
 
   def self.parents_list object, child_to_parents
+    puts ">>>>>>>>>>>>>>  #{__FILE__}:#{__LINE__} <<<<<<<<<<<<<<<<<\n"
+
     if parent = child_to_parents[object]
+      puts ">>>>>>>>>>>>>>  #{__FILE__}:#{__LINE__} <<<<<<<<<<<<<<<<<\n"
+      @objects = Set.new
       [].tap do |parents|
         while parent
+          puts ">>>>>>>>>>>>>>  #{__FILE__}:#{__LINE__} <<<<<<<<<<<<<<<<<\n"
           parents.unshift parent
           parent = child_to_parents[parent]
+          break if @objects.include?(parent)
+          @objects << parent
         end
       end
     end
   end
 
   def self.object_prefixes object, child_to_parents
+    puts ">>>>>>>>>>>>>>  #{__FILE__}:#{__LINE__} <<<<<<<<<<<<<<<<<\n"
     parents = parents_list object, child_to_parents
-
+    puts ">>>>>>>>>>>>>>  #{__FILE__}:#{__LINE__} <<<<<<<<<<<<<<<<<\n"
     if parents.present?
       root = parents.shift
       prefixes = [underscore_name(root)]
---
+++ spec/dummy/app/models/case.rb
@@ -0,0 +1,10 @@
+class Case
+  include ActiveModel::Model
+
+  attr_accessor :name
+  attr_accessor :state_machine
+  validates_presence_of :name
+
+
+
+end
