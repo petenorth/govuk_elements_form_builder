2d6e1c6181337c2724ffb5dadf32fe35f317deb3
Peter Yates <peter.yates@graphia.co.uk>
2018-12-20 15:47:55 +0000

    Squash some warnings regarding deprecated settings
    Specifically, the following setting  which has had its `false` setting
    deprecated:
    
    ActiveRecord::ConnectionAdapters::SQLite3Adapter.represent_boolean_as_integer
    
    For more information see the guide:
    
    https://edgeguides.rubyonrails.org/configuring.html#configuring-active-record

--- spec/dummy/config/application.rb
+++ spec/dummy/config/application.rb
@@ -27,6 +27,8 @@ module Dummy
 
     # Do not swallow errors in after_commit/after_rollback callbacks.
     # config.active_record.raise_in_transactional_callbacks = true
+
+    Rails.application.config.active_record.sqlite3.represent_boolean_as_integer = true
   end
 end
 
